8b94ea1da42b2891f092eca34a4449c982702b8d
Aaron Rotem <aaronrotem@gmail.com>
2018-06-01 10:57:01 -0400

    commented out view engine

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ var bodyParser = require("body-parser");
 
 
 var app = express();
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
