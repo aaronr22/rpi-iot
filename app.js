69d838e45480e93814870410284002cacd8793ee
Aaron Rotem <aaronrotem@gmail.com>
2018-07-06 01:14:34 -0400

    added env var

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(express.static(path.join(__dirname, '/')));
 app.set("view engine", "ejs");
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+//app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: process.env.seshsecret })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
